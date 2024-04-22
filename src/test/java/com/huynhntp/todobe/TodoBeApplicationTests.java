0d07c3b2c03cdb71da48c447bef75f9c8df17d39
huynh-ntp <phihuynh120120@gmail.com>
2024-04-22 23:23:09 +0700

    'init'

---
+++ src/test/java/com/huynhntp/todobe/TodoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huynhntp.todobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
